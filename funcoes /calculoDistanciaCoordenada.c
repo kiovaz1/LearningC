ec67ac4bc03cad0a10f0788ffaad568a92d9aff2
Caio Vasconcelos <caiovasconcelos01@live.com>
2024-10-09 16:54:59 -0300

    Update calculoDistanciaCoordenada.c

--- funcoes /calculoDistanciaCoordenada.c
+++ funcoes /calculoDistanciaCoordenada.c
@@ -9,69 +9,36 @@ void limparConsole() {
     #endif
 }
 
-void sacarDinheiro(int valor) {
-    int notas100, notas50, notas20, notas10;
+void calculoDistancia(float x1, float x2 , float y1 , float y2){
+    float distanciaX ;
+    distanciaX = x2 - x1 ;
+    float distanciaY ;
+    distanciaY = y2 - y1 ;
 
-    notas100 = valor / 100;
-    valor = valor % 100;
+    printf("A distancia é: X --> %.2f e Y é --> %.2f",distanciaX,distanciaY);
 
-    notas50 = valor / 50;
-    valor = valor % 50;
-
-    notas20 = valor / 20;
-    valor = valor % 20;
-
-    notas10 = valor / 10;
-
-    if (notas100 > 0) {
-        printf("Notas de 100: %d\n", notas100);
-    }
-    if (notas50 > 0) {
-        printf("Notas de 50: %d\n", notas50);
-    }
-    if (notas20 > 0) {
-        printf("Notas de 20: %d\n", notas20);
-    }
-    if (notas10 > 0) {
-        printf("Notas de 10: %d\n", notas10);
-    }
-    if (notas100 == 0 && notas50 == 0 && notas20 == 0 && notas10 == 0) {
-        printf("Valor não pode ser sacado com as notas disponíveis.\n");
-    }
 }
 
-int main() {
-    int continuar;
-    
-    do {
-        limparConsole();
-        int valor;
-
-        printf("Quanto você desejaria sacar?\n");
-        scanf("%d", &valor);
-        limparConsole();
-        if (valor <= 0) {
-            printf("Por favor, insira um valor positivo.\n");
-            continue;
-        }
-
-        if (valor % 10 != 0) {
-            printf("Por favor, insira um valor que seja múltiplo de 10.\n");
-            printf("Deseja tentar novamente? (1 - Sim, 0 - Não): ");
-            scanf("%d", &continuar);
-            if (continuar == 0) {
-                break;
-            }
-            continue;
-        }
-
-        sacarDinheiro(valor);
-
-        printf("\nDeseja realizar outro saque? (1 - Sim, 0 - Não): ");
-        scanf("%d", &continuar);
-        
-    } while (continuar == 1);
+int main()
+{
+    float x1;
+    float x2;
+    float y1;
+    float y2;
     limparConsole();
-    printf("Obrigado por usar o caixa eletrônico!\n");
+    printf("Insira as coordenada X1:\n");
+    scanf("%f",&x1);
+    limparConsole();
+    printf("Insira as coordenada Y1:\n");
+    scanf("%f",&y1);
+    limparConsole();
+    printf("Insira as coordenada X2:\n");
+    scanf("%f",&x2);
+    limparConsole();
+    printf("Insira as coordenada Y2:\n");
+    scanf("%f",&y2);
+    limparConsole();
+    calculoDistancia(x1,x2,y1,y2);
+
     return 0;
 }
