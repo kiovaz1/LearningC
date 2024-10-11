9fe3cccd0bdf27726a2bccf56744d38e6e80e65c
Caio Vasconcelos <caiovasconcelos01@live.com>
2024-10-11 19:13:34 -0300

    Update vetorSalario.c

--- vetores&matrizes/vetorSalario.c
+++ vetores&matrizes/vetorSalario.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main (){
+int salarios[12] = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000};
+int resultado = 0;
+for (int i = 0; i < 12; i++ ){
+resultado += salarios[i];
+printf("Salario Mensal do indivíduo %d é %d.\n",i+1, salarios[i]);
+}
+
+printf("A soma total anual dos salarios é: %d\n", resultado) ;
+return 0;
+}
